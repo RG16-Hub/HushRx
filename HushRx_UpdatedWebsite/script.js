5b0adeb79093ced851582f5d51221f5c8bafd8c0
RG16-Hub <rafael.gamboa1@yahoo.com>
2025-03-12 07:20:08 -0600

    Add files via upload

---
+++ HushRx_UpdatedWebsite/script.js
@@ -0,0 +1,4 @@
+document.getElementById('consultForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you! Your consultation request has been submitted. Our team will contact you shortly.");
+});
