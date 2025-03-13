ff95d71dfd3c19d21137b62281a3bfd668a78ab2
RG16-Hub <rafael.gamboa1@yahoo.com>
2025-03-12 20:41:56 -0600

    Delete HushRx_UpdatedWebsite directory

--- HushRx_UpdatedWebsite/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('consultForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert("Thank you! Your consultation request has been submitted. Our team will contact you shortly.");
-});
